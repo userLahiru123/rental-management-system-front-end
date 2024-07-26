467c54433c5a02cbf18ca41967fce00e3086d200
userLahiru123 <thiwanka631@gmail.com>
2024-07-26 15:42:40 +0530

    item page design

---
+++ src/app/pages/rental/rental.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rental',
+  standalone: true,
+  imports: [],
+  templateUrl: './rental.component.html',
+  styleUrl: './rental.component.css'
+})
+export class RentalComponent {
+
+}
