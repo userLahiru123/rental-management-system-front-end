1ecb3042ae01dff260915a2d6fb340adc030cb10
userLahiru123 <thiwanka631@gmail.com>
2024-07-26 17:13:40 +0530

    customer transactions

--- src/app/pages/customer/customer.component.ts
+++ src/app/pages/customer/customer.component.ts
@@ -1,13 +1,92 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../component/nav/nav.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import Swal from 'sweetalert2';
+import { FormsModule, NgModel } from '@angular/forms';
+import { compileNgModule } from '@angular/compiler';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent,HttpClientModule,FormsModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
 export class CustomerComponent {
+
+  public customerObj = {
+    customerId:"",
+    name:"",
+    city:"",
+    contactNumber:""
+  }
+
+  constructor(private http: HttpClient){}
+
+  clearFields(){
+    this.customerObj.name="";
+    this.customerObj.customerId="";
+    this.customerObj.city="";
+    this.customerObj.contactNumber="";
+  }
+
+  addCustomer(){
+    this.http.post("http://localhost:8080/customer",this.customerObj).subscribe(
+      (data) => {
+        Swal.fire({
+          title: "Customer added!",
+          text: "You clicked the button!",
+          icon: "success"
+        });
+
+        this.clearFields();
+      }
+    )
+  }
+
+  deleteCustomer(){
+    this.http.delete("http://localhost:8080/customer/delete-by-id/"+this.customerObj.customerId).subscribe(
+      (data) => {
+        Swal.fire({
+          title: "Customer updated!",
+          text: "You clicked the button!",
+          icon: "success"
+        });
+
+        this.clearFields();
+      }
+    )
+  }
+
+  updateCustomer(){
+    this.http.post("http://localhost:8080/customer/update-by-id",this.customerObj).subscribe(
+      (data) => {
+        Swal.fire({
+          title: "Customer added!",
+          text: "You clicked the button!",
+          icon: "success"
+        });
+
+        this.clearFields();
+      }
+    )
+  }
+
+  searchCustomer(){
+    this.http.get("http://localhost:8080/customer/"+this.customerObj.customerId).subscribe(
+      (data) => {
+        if(data == null){
+          Swal.fire({
+            title: "Customer not found!",
+            text: "You clicked the button!",
+            icon: "question"
+          });
+        }else{
+          console.log(data);
+        }
+      }
+    )
+  }
+
 
 }
---
+++ src/app/service/CustomerService.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-customer',
+    standalone: true,
+    imports: [],
+  })
+  export class CustomerService {
+    public void
+  }
